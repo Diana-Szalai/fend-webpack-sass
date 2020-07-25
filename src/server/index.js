06665cd1fd4554e7c4690a99bda86c06f5385a58
Diana <diana.szalai@yahoo.com>
2020-07-25 18:14:27 +0300

    Setting up the Aylien API

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,16 @@
+const dotenv = require('dotenv');
+dotenv.config();
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+
+// Require the Aylien npm package
+var aylien = require("aylien_textapi");
+// You could call it aylienapi, or anything else
+var textapi = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY,
+  });
 var bodyParser = require('body-parser')
 var cors = require('cors')
 
